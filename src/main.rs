d0430a0253bccd860ba958cf1b6920ebdb316a19
Sean Murdoch <145845458+Wytchwulf@users.noreply.github.com>
2025-04-29 02:18:05 +0000

    Handling errors

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,35 @@
-use std::io::{stdin};
+use std::io::stdin;
 
 fn main() {
-    println!("Please enter F or C for unit type you wish to convert");
+    println!("Enter F (Farenheit) or C (Celcius) for unit type to convert");
 
     let mut unit = String::new();
 
-    stdin()
-        .read_line(&mut unit)
-        .expect("Unable to read line");
+    stdin().read_line(&mut unit).expect("Unable to read line");
 
-    println!("Please enter the value you wish to convert");
+    println!("Enter value to convert");
 
     let mut val = String::new();
 
-    stdin()
-        .read_line(&mut val)
-        .expect("Unable to read line");
+    stdin().read_line(&mut val).expect("Unable to read line");
 
-    let val: f32 = val.trim().parse().expect("Please enter a number");
+    let val: f64 = val.trim().parse().expect("Please enter a number");
 
     if unit.trim().eq_ignore_ascii_case("f") {
-        let conversion = ftoc(val);
-        println!("{}", conversion);
+        let conversion: f64 = ftoc(val);
+        println!("{:.2}°C", conversion);
+    } else if unit.trim().eq_ignore_ascii_case("c") {
+        let conversion: f64 = ctof(val);
+        println!("{:.2}°F", conversion)
     } else {
-        let conversion = ctof(val);
-        println!("{}", conversion)
+        println!("Invalid unit type")
     }
-
 }
 
-fn ftoc(f: f32) -> f32 {
+fn ftoc(f: f64) -> f64 {
     (f - 32.0) / 1.8
 }
 
-fn ctof(c: f32) -> f32 {
+fn ctof(c: f64) -> f64 {
     c * 1.8 + 32.0
 }
